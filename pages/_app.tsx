05a6ef8130fe7a3f129166c25d29d80f325e75da
Ingeun Kim <ingeun92@naver.com>
2022-07-28 11:27:50 +0900

    fix bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
 export default MyApp;
